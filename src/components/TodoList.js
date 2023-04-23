5aed88b047d771818ba95470a4d88d4de340f3e7
RonakFabian <ronakmanfabian@gmail.com>
2023-04-23 23:03:14 +0530

    Changes

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -45,6 +45,10 @@ const TodoList = ({ entryID, setEntryID }) =>
         window.open(url);
     }
 
+    let colors = ["peach", "blue", "green", "yellow"];
+    let btnColors = ["dark-peach", "dark-blue", "dark-green", "dark-yellow"];
+
+
 
     return (
         <>
@@ -68,20 +72,20 @@ const TodoList = ({ entryID, setEntryID }) =>
                     return (
                         <>
                             <div className="card-outer">
-                                <div className="btn btn-secondary number">
+                                <div className={"btn btn-secondary number " + btnColors[index % 4]}>
                                     <div>{index + 1 + " "}</div>
                                 </div>
 
                                 <div class="card w-100" key={doc.id}>
-                                    <div class="card-body w-100 ">
+                                    <div class={colors[index % 4] + " card-body w-100"}>
                                         <div className="d-flex flex-wrap w-100">
                                             <div className="col">
                                                 <h1 class="card-title ">{doc.taskName}</h1>
                                                 <p class="card-text">{doc.description}</p>
                                             </div>
                                             <div className="flex-column-reverse">
-                                                <button className='btn btn-primary button-new  pb-2 br-2' onClick={() => locate(doc)}><MdLocationPin /></button>
-                                                <button className='btn btn-primary button-new  pb-2 br-2' onClick={(e) => deleteHandler(doc.id)}><MdDelete /></button>
+                                                <button className={'btn btn-primary button-new  pb-2 br-2 ' + btnColors[index % 4]} onClick={() => locate(doc)}><MdLocationPin /></button>
+                                                <button className={'btn btn-primary button-new  pb-2 br-2 ' + btnColors[index % 4]} onClick={(e) => deleteHandler(doc.id)}><MdDelete /></button>
                                                 {/* <button className='btn btn-primary pb-2' onClick={() => { setEntryID(doc.id); toggleUpdate(); }}><MdEdit /></button> */}
                                             </div>
                                         </div>
